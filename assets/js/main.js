e8e5d52b61bd98d2063cd363a59d64ab76cc793c
jaimeHMol <jaimehmol@yahoo.com>
2019-09-08 22:08:55 -0300

    Working on Who we are section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 /*
+	Base code from:
 	TXT by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
@@ -72,3 +73,48 @@
 				});
 
 })(jQuery);
+
+
+/* jaimeHMol: Custom functions */
+function changeWhoWeAreSection(title, description){
+	var display = document.getElementById('whoWeAreTitle');
+	display.innerHTML = "";
+	display.innerHTML = title;
+
+	var display = document.getElementById('whoWeAreDescription');
+	display.innerHTML = "";
+	display.innerHTML = description;
+
+	var display = document.getElementById('whoWeAreInitial');
+	display.style.display = "none";
+	var display = document.getElementById('whoWeAreContainer');
+	display.style.display = "block";
+	// var display = document.getElementById('whoWeAreInitial');
+	// display.style.visibility = "hidden";
+	// var display = document.getElementById('whoWeAreContainer');
+	// display.style.visibility = "visible";
+}
+
+function changeBackWhoWeAreSection(){
+	var display = document.getElementById('whoWeAreContainer');
+	display.style.display = "none";
+	var display = document.getElementById('whoWeAreInitial');
+	display.style.display = "block";
+	// var display = document.getElementById('whoWeAreContainer');
+	// display.style.visibility = "hidden";
+	// var display = document.getElementById('whoWeAreInitial');
+	// display.style.visibility = "visible";
+
+
+	// var display = document.getElementById('whoWeAreTitle');
+	// display.innerHTML = "";
+	// display.innerHTML = "Quienes somos?";
+	//
+	// var display = document.getElementById('whoWeAreSubTitle');
+	// display.innerHTML = "";
+	// display.innerHTML = "La gente detrás de Reeb";
+	//
+	// var display = document.getElementById('whoWeAreDescription');
+	// display.innerHTML = "";
+	// display.innerHTML = "Somos un grupo de jovenes aficionados a la cerveza que insatisfechos con la oferta local de cervezas artesanales decidió crear una propia...&lt;br/&gt; &lt;img src='images/ourOwnBeer.jpg' alt='Nuestra propia cerveza' width='40%' height='40%'&gt; &lt;br/&gt; Somos apasionados y queremos compartir nuestra pasión cervecera contigo.";
+}
